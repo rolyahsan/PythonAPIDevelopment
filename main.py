519020d3d5d0efb915f71b77e7ffedeeb148b5f6
Roly Hr <hafizurroly@gmail.com>
2021-11-18 21:58:22 -0500

    Added a list

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ class Post(BaseModel):
     salary: float
     published: bool = False
     rating: Optional[int] = None
+
+
+my_posts = [{"post_id": 1, "job_title": "Software QA Engineer", "salary": 125000, "published": False},
+            {"post_id": 2, "job_title": "QA Analyst", "salary": 98000, "rating": 4},
+            {"post_id": 3, "job_title": "Manual Tester", "salary": 75000, "published": True, "rating": 5}]

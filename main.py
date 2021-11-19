f563157546c5e89f1b39441cc9f43f5330d6bbf9
Roly Hr <hafizurroly@gmail.com>
2021-11-18 22:28:51 -0500

    Added post methods

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+def find_post(post_id):
+    for p in my_posts:
+        if p['post_id'] == post_id:
+            return p
+
+
 my_posts = [{"post_id": 1, "job_title": "Software QA Engineer", "salary": 125000, "published": False},
             {"post_id": 2, "job_title": "QA Analyst", "salary": 98000, "rating": 4},
             {"post_id": 3, "job_title": "Manual Tester", "salary": 75000, "published": True, "rating": 5}]
@@ -22,3 +28,14 @@ my_posts = [{"post_id": 1, "job_title": "Software QA Engineer", "salary": 125000
 @hrrapi.get("/")
 def root():
     return {"Welcome": "to hrrAPI"}
+
+
+@hrrapi.get("/posts")
+def get_post():
+    return {"message": my_posts}
+
+
+@hrrapi.get("/posts/{post_id}")
+def get_post(post_id):
+    post = find_post(int(post_id))
+    return {"message": post}
